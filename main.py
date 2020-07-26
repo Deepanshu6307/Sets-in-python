2304b8b097c42c5775b52470fd71599281579a75
Deepanshu Gautam <ishugautam29@gmail.com>
2020-07-26 10:34:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+library1 = {'Harry Potter', 'Game of thrones', 'Hunger games'}
+library2 = {'Harry Potter', 'Lord of the rings'} 
+
+All_books_in_town = library1.difference(library2)
+
+print(All_books_in_town)
